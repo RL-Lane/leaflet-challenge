5bdb1981b0cee54f6e3d2daf601fb96602f9775a
Robert Lane <RobertLaneJr@gmail.com>
2022-06-29 23:15:06 -0500

    color scales functioning

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,10 +3,11 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
 
-    console.log(data)
+    // console.log(data)
     // Once we get a response, send the data.features object to the createFeatures function.
     createFeatures(data.features);
   });
+  
   
   function createFeatures(earthquakeData) {
   
@@ -14,18 +15,61 @@ d3.json(queryUrl).then(function (data) {
     // Give each feature a popup that describes the place and time of the earthquake.
     function onEachFeature(feature, layer) {
       layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+      
     }
   
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
     // Run the onEachFeature function once for each piece of data in the array.
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
+
+    //old version
+    // var earthquakes = L.geoJSON(earthquakeData, {
+    //   onEachFeature: onEachFeature
+    // });
+
+    var earthquakes = L.geoJSON
+    (
+      earthquakeData, 
+      {
+        pointToLayer: function (feature, latlng) 
+          {
+          return L.circleMarker(latlng, geojsonMarkerOptions(feature));
+          }
+      }
+    );
+
+
+    function geojsonMarkerOptions(feature) {
+      console.log(feature.geometry.coordinates[2]);
+      return {      
+        radius: feature.properties.mag *5,
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      }
+  };
   
     // Send our earthquakes layer to the createMap function/
     createMap(earthquakes);
   }
+
+
   
+  function getColor(d) {
+    return d > 30   ? '#0A2F51' :
+           d > 20   ? '#0F596B' :
+           d > 15   ? '#16837A' :
+           d > 10   ? '#1D9A6C' :
+           d > 5    ? '#56B870' :
+           d > 2.5  ? '#99D492' :
+           d > 0    ? '#99D492' :
+                      '#DEEDCF';
+  }
+
+
+
+
   function createMap(earthquakes) {
   
     // Create the base layers.
